432281f1d006a3c4b5eeea5f8fdbf08546e8b0f8
Kkathyf <fengganlin2@gmail.com>
2024-10-08 00:01:22 -0400

    finish

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,13 @@ module.exports = function override(config, env) {
       test: /\.worker\.js$/,  
       use: { loader: 'worker-loader' }
     });
-  
+
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // Preserve existing fallbacks
+      "path": false,
+      "fs": false,
+      "crypto": false
+    };
+
     return config;
   };
